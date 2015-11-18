311cf2c7f1ee17ad75250982b098099ad24fc6d6
v <v@vs-iMac.local>
2015-11-18 15:22:01 -0500

    levels 1-7 and 1-8

---
+++ Assets/Scripts/LevelControllers/Level17Controller.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Level17Controller : LevelController {
+	public override void Invoke(){
+		GameObject bubbleObject = Instantiate (Resources.Load ("SpeechBubble"), Vector3.zero, Quaternion.identity) as GameObject;
+		SpeechBubble speechBubble = bubbleObject.GetComponent<SpeechBubble>();
+		speechBubble.setWidth = HUDHelpers.speechWidth;
+		speechBubble.setHeight = HUDHelpers.speechHeight;
+		speechBubble.textToDisplay = new string[3];
+		speechBubble.textToDisplay[0] = "Did you know that some colors are opposites? red and green are considered opposites.";
+		speechBubble.textToDisplay[1] = "interesting things happen when opposites are arranged together.";
+		speechBubble.textToDisplay[2] = "try surrounding tiles with their opposites to see the effects.";
+		speechBubble.dismissable = true;
+		speechBubble.freezesGameOnDisplay = true;
+		speechBubble.transform.position = Camera.main.WorldToScreenPoint(HUDHelpers.speechPosition);
+	}
+}
+
+
---
+++ Assets/Scripts/LevelControllers/Level18Controller.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Level18Controller : LevelController {
+	public override void Invoke(){
+		GameObject bubbleObject = Instantiate (Resources.Load ("SpeechBubble"), Vector3.zero, Quaternion.identity) as GameObject;
+		SpeechBubble speechBubble = bubbleObject.GetComponent<SpeechBubble>();
+		speechBubble.setWidth = HUDHelpers.speechWidth;
+		speechBubble.setHeight = HUDHelpers.speechHeight;
+		speechBubble.textToDisplay = new string[4];
+		speechBubble.textToDisplay[0] = "there are no bombs on the field. quite a predicament...";
+		speechBubble.textToDisplay[1] = "you can make your own bombs by surrounding a color with its opposites.";
+		speechBubble.textToDisplay[2] = "the pattern can either be plus shaped...";
+		speechBubble.textToDisplay[3] = "... or shaped like a complete square. try them out!";
+		speechBubble.dismissable = true;
+		speechBubble.freezesGameOnDisplay = true;
+		speechBubble.transform.position = Camera.main.WorldToScreenPoint(HUDHelpers.speechPosition);
+	}
+}
+
+
