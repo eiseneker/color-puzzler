230f1c1bba1359af48f14006f240c034d0fba4d9
v <v@vs-iMac.local>
2015-11-23 15:25:27 -0500

    stability hud

---
+++ Assets/StabilityHUD.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class StabilityHUD : MonoBehaviour {
+
+	Text text;
+
+	// Use this for initialization
+	void Start () {
+		text = transform.Find ("Percentage").GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		text.text = (Cluster.colorStability * 100).ToString () + "%";
+	}
+}
